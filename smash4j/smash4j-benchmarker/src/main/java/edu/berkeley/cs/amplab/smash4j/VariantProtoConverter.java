5ca8540237b01daf8f40698dcbb414d4a883d813
Kelly Westbrooks <kwestbrooks@google.com>
2014-05-30 08:57:59 -0700

    Fixed NPE problem in VariantProtoConverter.java

--- smash4j/smash4j-benchmarker/src/main/java/edu/berkeley/cs/amplab/smash4j/VariantProtoConverter.java
+++ smash4j/smash4j-benchmarker/src/main/java/edu/berkeley/cs/amplab/smash4j/VariantProtoConverter.java
@@ -4,12 +4,14 @@ import com.google.api.services.genomics.model.Call;
 import com.google.api.services.genomics.model.Variant;
 import com.google.common.base.Function;
 import com.google.common.base.Functions;
+import com.google.common.base.Optional;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.Iterables;
 import edu.berkeley.cs.amplab.smash4j.Smash4J.VariantProto;
 import edu.berkeley.cs.amplab.vcfparser.VcfRecord;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
@@ -50,20 +52,25 @@ public abstract class VariantProtoConverter<X> {
       new VariantProtoConverter<VcfRecord>() {
         @Override public VariantProto convert(final VcfRecord record) {
           return VariantProto.newBuilder()
-              .addAllAlternateBases(record.alt())
-              .addAllCall(Iterables.transform(record.samples(), Functions.compose(CONVERT_MULTIMAP,
-                  new Function<List<String>, Map<String, List<String>>>() {
-                    @Override public Map<String, List<String>> apply(List<String> call) {
-                      ImmutableMap.Builder<String, List<String>> map = ImmutableMap.builder();
-                      for (Iterator<String>
-                          keys = record.format().iterator(), values = call.iterator();
-                          keys.hasNext() && values.hasNext();
-                          map.put(keys.next(), Arrays.asList(values.next().split(","))));
-                      return map.build();
-                    }
-                  })))
+              .addAllAlternateBases(emptyForNull(record.alt()))
+              .addAllCall(
+                  Iterables.transform(
+                      emptyForNull(record.samples()),
+                      Functions.compose(
+                          CONVERT_MULTIMAP,
+                          new Function<List<String>, Map<String, List<String>>>() {
+                            @Override public Map<String, List<String>> apply(List<String> call) {
+                              ImmutableMap.Builder<String, List<String>>
+                                  map = ImmutableMap.builder();
+                              for (Iterator<String>
+                                  keys = record.format().iterator(), values = call.iterator();
+                                  keys.hasNext() && values.hasNext();
+                                  map.put(keys.next(), Arrays.asList(values.next().split(","))));
+                              return map.build();
+                            }
+                          })))
               .setContig(record.chrom())
-              .addAllNames(record.ids())
+              .addAllNames(emptyForNull(record.ids()))
               .setPosition(record.pos())
               .setReferenceBases(record.ref())
               .setInfo(CONVERT_MULTIMAP.apply(record.info()))
@@ -75,16 +82,20 @@ public abstract class VariantProtoConverter<X> {
       new VariantProtoConverter<Variant>() {
         @Override public VariantProto convert(Variant variant) {
           return VariantProto.newBuilder()
-              .addAllAlternateBases(variant.getAlternateBases())
-              .addAllCall(Iterables.transform(variant.getCalls(), CALL_TO_MULTIMAP))
+              .addAllAlternateBases(emptyForNull(variant.getAlternateBases()))
+              .addAllCall(Iterables.transform(emptyForNull(variant.getCalls()), CALL_TO_MULTIMAP))
               .setContig(variant.getContig())
               .setInfo(CONVERT_MULTIMAP.apply(variant.getInfo()))
-              .addAllNames(variant.getNames())
+              .addAllNames(emptyForNull(variant.getNames()))
               .setPosition(variant.getPosition())
               .setReferenceBases(variant.getReferenceBases())
               .build();
         }
       };
+
+  private static <X> List<X> emptyForNull(List<X> list) {
+    return Optional.fromNullable(list).or(Collections.<X>emptyList());
+  }
 
   public abstract VariantProto convert(X object);
 }
