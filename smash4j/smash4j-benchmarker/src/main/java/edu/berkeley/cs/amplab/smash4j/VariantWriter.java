d732090a107f6a27a831eb601fa46508a3178821
Kelly Westbrooks <kwestbrooks@google.com>
2014-06-03 13:35:14 -0700

    Add VariantWriter.java

---
+++ smash4j/smash4j-benchmarker/src/main/java/edu/berkeley/cs/amplab/smash4j/VariantWriter.java
@@ -0,0 +1,43 @@
+package edu.berkeley.cs.amplab.smash4j;
+
+import com.google.common.base.Function;
+import com.google.common.base.Functions;
+import com.google.common.collect.FluentIterable;
+
+import edu.berkeley.cs.amplab.smash4j.Smash4J.VariantProto;
+
+import java.io.OutputStream;
+
+public class VariantWriter {
+
+  public static VariantWriter create(OutputStream out) {
+    return new VariantWriter(out, Functions.<Iterable<VariantProto>>identity());
+  }
+
+  private final OutputStream out;
+  private final Function<? super Iterable<VariantProto>, ? extends Iterable<VariantProto>> transformer;
+
+  private VariantWriter(
+      OutputStream out,
+      Function<? super Iterable<VariantProto>, ? extends Iterable<VariantProto>> transformer) {
+    this.out = out;
+    this.transformer = transformer;
+  }
+
+  public VariantWriter transform(Function<? super Iterable<VariantProto>, ? extends Iterable<VariantProto>> transformer) {
+    return new VariantWriter(out, Functions.compose(transformer, this.transformer));
+  }
+
+  public VariantWriter writeVariants(VariantScanner scanner) throws Exception {
+    return scanner.scan(
+        new VariantScanner.Callback<VariantWriter>() {
+          @Override public VariantWriter accept(FluentIterable<VariantProto> variants)
+              throws Exception {
+            for (VariantProto variant : variants) {
+              variant.writeDelimitedTo(out);
+            }
+            return VariantWriter.this;
+          }
+        });
+  }
+}
