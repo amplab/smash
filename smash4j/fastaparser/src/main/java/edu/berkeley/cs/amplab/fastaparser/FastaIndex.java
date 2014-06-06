50e2a828a376ace825cd66e3167e2f397d6c46b2
Kelly Westbrooks <kwestbrooks@google.com>
2014-06-06 13:42:43 -0700

    s/open/read/

--- smash4j/fastaparser/src/main/java/edu/berkeley/cs/amplab/fastaparser/FastaIndex.java
+++ smash4j/fastaparser/src/main/java/edu/berkeley/cs/amplab/fastaparser/FastaIndex.java
@@ -69,6 +69,10 @@ public class FastaIndex {
     private static final Pattern PATTERN = Pattern.compile(
         "([^\t]+?)\t(0|[1-9][0-9]*?)\t(0|[1-9][0-9]*?)\t(0|[1-9][0-9]*?)\t(0|[1-9][0-9]*?)");
 
+    public static Entry create(String name, long length, long offset, int bases, int bytes) {
+      return new Entry(name, length, offset, bases, bytes);
+    }
+
     public static Entry parse(String line) {
       Matcher matcher = PATTERN.matcher(line);
       if (!matcher.matches()) {
@@ -83,15 +87,11 @@ public class FastaIndex {
           Integer.parseInt(matcher.group(5)));
     }
 
-    public static Entry create(String name, long length, long offset, int bases, int bytes) {
-      return new Entry(name, length, offset, bases, bytes);
-    }
-
-    private final String name;
-    private final long length;
-    private final long offset;
     private final int bases;
     private final int bytes;
+    private final long length;
+    private final String name;
+    private final long offset;
 
     private Entry(String name, long length, long offset, int bases, int bytes) {
       this.name = name;
@@ -156,63 +156,65 @@ public class FastaIndex {
         return (IOException) super.getCause();
       }
     }
-    try {
-      try (final BufferedReader in = new BufferedReader(new FileReader(fastaFile))) {
-        ImmutableSortedSet.Builder<FastaIndex.Entry> entries = ImmutableSortedSet.naturalOrder();
-        String name = null;
-        long length = 0;
-        long offset1 = 0;
-        long offset2 = 0;
-        int bases = 0;
-        for (
-            PeekingIterator<String> iterator = Iterators.peekingIterator(
-                new AbstractIterator<String>() {
-                  @Override protected String computeNext() {
-                    try {
-                      String line = in.readLine();
-                      return null == line ? endOfData() : line;
-                    } catch (IOException e) {
-                      throw new ExceptionWrapper(e);
-                    }
+    try (final BufferedReader in = new BufferedReader(new FileReader(fastaFile))) {
+      ImmutableSortedSet.Builder<FastaIndex.Entry> entries = ImmutableSortedSet.naturalOrder();
+      String name = null;
+      long length = 0;
+      long offset1 = 0;
+      long offset2 = 0;
+      int bases = 0;
+      for (
+          PeekingIterator<String> iterator = Iterators.peekingIterator(
+              new AbstractIterator<String>() {
+                @Override protected String computeNext() {
+                  try {
+                    String line = in.readLine();
+                    return null == line ? endOfData() : line;
+                  } catch (IOException e) {
+                    throw new ExceptionWrapper(e);
                   }
-                });
-            iterator.hasNext();) {
-          String line = iterator.next();
-          int lineLength = line.length();
-          offset2 += lineLength + 1;
-          if (line.startsWith(">")) {
-            if (null != name) {
-              entries.add(FastaIndex.Entry.create(name, length, offset1, bases, bases + 1));
-            }
-            name = line.substring(1).split("\\p{Space}+?")[0];
-            length = 0;
-            offset1 = offset2;
-            bases = 0;
-          } else {
-            if (0 == bases) {
-              bases = lineLength;
-            } else if (bases != lineLength
-                && iterator.hasNext()
-                && !iterator.peek().startsWith(">")) {
-              throw new IllegalStateException(String.format(
-                  "Inconsistent line lengths at contig \"%s\", offset %d",
-                  name,
-                  offset2 - lineLength - 1));
-            }
-            length += lineLength;
+                }
+              });
+          iterator.hasNext();) {
+        String line = iterator.next();
+        int lineLength = line.length();
+        offset2 += lineLength + 1;
+        if (line.startsWith(">")) {
+          if (null != name) {
+            entries.add(FastaIndex.Entry.create(name, length, offset1, bases, bases + 1));
           }
+          name = line.substring(1).split("\\p{Space}+?")[0];
+          length = 0;
+          offset1 = offset2;
+          bases = 0;
+        } else {
+          if (0 == bases) {
+            bases = lineLength;
+          } else if (bases != lineLength
+              && iterator.hasNext()
+              && !iterator.peek().startsWith(">")) {
+            throw new IllegalStateException(String.format(
+                "Inconsistent line lengths at contig \"%s\", offset %d",
+                name,
+                offset2 - lineLength - 1));
+          }
+          length += lineLength;
         }
-        if (null != name) {
-          entries.add(FastaIndex.Entry.create(name, length, offset1, bases, bases + 1));
-        }
-        return FastaIndex.create(entries.build());
       }
+      if (null != name) {
+        entries.add(FastaIndex.Entry.create(name, length, offset1, bases, bases + 1));
+      }
+      return FastaIndex.create(entries.build());
     } catch (ExceptionWrapper e) {
       throw e.getCause();
     }
   }
 
-  public static FastaIndex open(File file) throws IOException {
+  public static FastaIndex create(SortedSet<Entry> entries) {
+    return new FastaIndex(entries);
+  }
+
+  public static FastaIndex read(File file) throws IOException {
     try (Reader in = new FileReader(file)) {
       return read(in);
     }
@@ -231,10 +233,6 @@ public class FastaIndex {
       entries.add(Entry.parse(line));
     }
     return create(entries.build());
-  }
-
-  public static FastaIndex create(SortedSet<Entry> entries) {
-    return new FastaIndex(entries);
   }
 
   private final SortedSet<Entry> entries;
