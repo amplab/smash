1933420f4ae081f0bf68fd47cf0eed2b063b8701
Kelly Westbrooks <kwestbrooks@google.com>
2014-07-02 15:10:23 -0700

    Update main method

--- smash4j/src/main/java/edu/berkeley/cs/amplab/smash4j/Main.java
+++ smash4j/src/main/java/edu/berkeley/cs/amplab/smash4j/Main.java
@@ -1,10 +1,13 @@
 package edu.berkeley.cs.amplab.smash4j;
 
 import com.google.common.base.Optional;
+import com.google.common.collect.FluentIterable;
 
 import edu.berkeley.cs.amplab.smash4j.CommandDispatcher.MainCommand;
+import edu.berkeley.cs.amplab.smash4j.fasta.FastaReader;
 
 import java.io.File;
+import java.util.Map;
 import java.util.prefs.Preferences;
 
 public class Main {
@@ -15,7 +18,58 @@ public class Main {
       PREFERENCES_PATH = PREFERENCES.absolutePath(),
       PROGRAM_NAME = "AMPLab-SMaSH4J/0.1";
 
-  private static void main(MainCommand command) throws Exception {
+  private static void main(final MainCommand command) throws Exception {
+    File reference = command.referenceFasta();
+    Optional<File> index = command.referenceFastaIndex();
+    output(
+        (index.isPresent() ? FastaReader.create(reference, index.get()) : FastaReader.create(reference)).read(
+            new FastaReader.Callback<Map<String, VariantEvaluator.ContigStats>>() {
+
+              private VariantScanner createScanner(Optional<String> callset, Optional<File> vcf) {
+                if (callset.isPresent() && !vcf.isPresent()) {
+                  return VariantScanner.fromCallset(callset.get());
+                } else if (!callset.isPresent() && vcf.isPresent()) {
+                  return VariantScanner.fromVcfFile(vcf.get());
+                } else {
+                  throw new IllegalStateException();
+                }
+              }
+
+              @Override public Map<String, VariantEvaluator.ContigStats> read(
+                  Map<String, Integer> info,
+                  final FastaReader.FastaFile reference) throws Exception {
+                return createScanner(command.trueCallset(), command.trueVcf()).scan(
+                    new VariantScanner.Callback<Map<String, VariantEvaluator.ContigStats>>() {
+                      @Override public Map<String, VariantEvaluator.ContigStats> scan(
+                          final FluentIterable<Variant> trueVariants) throws Exception {
+                        return createScanner(command.predictedCallset(), command.predictedVcf()).scan(
+                            new VariantScanner.Callback<Map<String, VariantEvaluator.ContigStats>>() {
+                              @Override public Map<String, VariantEvaluator.ContigStats> scan(
+                                  FluentIterable<Variant> predictedVariants) throws Exception {
+                                int maxIndelSize = command.maxIndelSize().or(50);
+                                Normalizer normalizer = Normalizer.create(maxIndelSize, reference);
+                                return VariantEvaluator.builder()
+                                    .setMaxIndexSize(maxIndelSize)
+                                    .setMaxSvBreakpointDistance(
+                                        command.maxSvBreakpointDistance().or(100))
+                                    .setMaxVariantLengthDifference(
+                                        command.maxVariantLengthDifference().or(100))
+                                    .setReference(reference)
+                                    .setRescueWindowSize(command.rescueWindowSize().or(50))
+                                    .build()
+                                    .evaluate(
+                                        normalizer.normalize(trueVariants),
+                                        normalizer.normalize(predictedVariants));
+                              }
+                            });
+                      }
+                    });
+              }
+            }));
+  }
+
+  private static void output(Map<String, VariantEvaluator.ContigStats> contigStats) {
+    // TODO
   }
 
   public static void main(String[] args) throws Exception {
